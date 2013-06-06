10caa4a113321eed02aa3a9511b11af1519aa5e7
Alan Shaw <alan@freestyle-developments.co.uk>
2013-06-06 09:17:07 +0100

    Initial working version

--- client/js/main.js
+++ client/js/main.js
@@ -1,16 +1,36 @@
 Meteor.subscribe("messages")
 
+// Get the messages for the template
 Template.msgs.msgs = function () {
   return Messages.find().fetch()
 }
 
+// Asciify the message text when the template is rendered
 Template.msg.rendered = function () {
   var pre = $(this.find("pre"))
-  Meteor.call("asciify", pre.text(), pre.data("font"), function (er, text) {
-    pre.text(text)
-  })
+  if (!pre.data("asciified")) {
+    Meteor.call("asciify", pre.text(), pre.data("font"), function (er, text) {
+      pre.attr("data-asciified", true).text(text)
+      scrollToBottom()
+    })
+  }
 }
 
+// Scroll to bottom of the page (debounced)
+var scrollToBottom = (function() {
+  var scheduled = false
+  return function() {
+    if (!scheduled) {
+      scheduled = true
+      Meteor.setTimeout(function() {
+        scheduled = false
+        $("html, body").animate({scrollTop: $(document).height()}, 1000)
+      }, 250)
+    }
+  }
+})()
+
+// Get or generate the user's handle
 Template.input.rendered = function () {
   var handle = localStorage && localStorage.getItem("handle")
   if (handle) {
@@ -22,19 +42,28 @@ Template.input.rendered = function () {
   }
 }
 
+// Send a message by inserting into the Messages collection
+function sendMsg () {
+  Messages.insert({
+      handle: $("#handle").val()
+    , msg: $("#msg").val()
+    , font: $("#font").val()
+    , created: moment().toDate().getTime()
+  })
+  $("#msg").val("")
+}
+
+// Events for sending messages and saving handle to localStorage
 Template.input.events({
-  "click #send": function () {
-    Messages.insert({
-        handle: $("#handle").val()
-      , msg: $("#msg").val()
-      , font: $("#font").val()
-      , created: moment().toDate().getTime()
-    })
-    $("#msg").val("")
-  },
+  "click #send": sendMsg,
   "keyup #handle": function () {
     if (localStorage) {
       localStorage.setItem("handle", $("#handle").val())
+    }
+  },
+  "keypress #msg": function (event) {
+    if (event.which == 13) {
+      sendMsg()
     }
   }
 })
