b5c23496c64dd818720eac587df512eb45e2cf0e
olizilla <oli@zilla.org.uk>
2013-06-09 20:02:22 +0100

    Minimal chat channel implementation

--- server/main.js
+++ server/main.js
@@ -27,6 +27,6 @@ Meteor.methods({
   }
 })
 
-Meteor.publish("messages", function () {
-  return Messages.find({}, {sort: [['created', 'desc']]})
+Meteor.publish("messages", function (opts) {
+  return Messages.find({channel: opts.channel}, {limit: opts.limit, sort: [['created', 'desc']]})
 })
