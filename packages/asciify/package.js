d494897ad0c0e8992bc32fb6b1ce4f2fb773e5fd
Alan Shaw <alan@freestyle-developments.co.uk>
2014-02-28 08:53:29 +0000

    Update to latest meteor

--- packages/asciify/package.js
+++ packages/asciify/package.js
@@ -4,5 +4,6 @@ Package.describe({summary: "Plain text awesomizer."})
 Npm.depends({"asciify": "1.3.2"})
 
 Package.on_use(function (api) {
+  api.export("asciify")
   api.add_files("asciify.js", "server")
 })
